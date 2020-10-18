9df1040282df1bf61f239dca3ff8ee21175ecbd0
Sivert Schou Olsen <sivertschou@hotmail.com>
2020-10-18 21:25:12 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const App = () => {
+  return <div>Halla;)</div>;
+};
+
+export default App;
---
+++ src/popup.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import App from "./App";
+
+var mountNode = document.getElementById("popup");
+ReactDOM.render(<App />, mountNode);
