e33d7876cb009c19e7baa250338dc12a6f5b9782
Sivert Schou Olsen <sivertschou@hotmail.com>
2020-10-18 22:47:01 +0200

    Add react default styling

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-
-const App = () => {
-  return <div>Halla;)</div>;
-};
-
-export default App;
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import App from "./App";
+import App from "./components/App/App";
+import "./popup.css";
 
 var mountNode = document.getElementById("popup");
 ReactDOM.render(<App />, mountNode);
